3900b63ca41c439a9348274715de6804fb89297c
Victoriabunmi72 <bhummhie97@gmail.com>
2023-08-16 22:30:10 +0100

    exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+""" python redis practice scripts """
+import redis
+from uuid import uuid4
+from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """ Count the number of times a method is called
+    """
+    key = method.__qualname__  # Generate a unique key
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Callable:
+        """ Wrapper function
+        """
+        self._redis.incr(key)  # Increment the key
+        return method(self, *args, **kwargs)  # Call the method
+    return wrapper
+
+
+class Cache:
+    """ Cache Storage class
+    """
+
+    def __init__(self) -> None:
+        self._redis: redis.Redis = redis.Redis()  # Redis client instance
+        self._redis.flushdb()
+
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key: str = str(uuid4())  # Generate a unique key
+        self._redis.set(key, data)  # Store the data in Redis
+        return key  # Return the key for later retrieval
+
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> Union[str, bytes, int, float]:
+        """ Get a value from Redis and convert it to the desired format
+        """
+        # get the data from redis
+        data = self._redis.get(key)
+        # if a function was passed, apply it to the data
+        if fn:
+            data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ Convert data to str
+        """
+        result = self._redis.get(key)
+        return str(result.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """ Convert data to int
+        """
+        result = self._redis.get(key)
+        return int(result.decode("utf-8"))
